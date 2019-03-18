e29f2ce4f341c64a3b13bc3fa30688a33ca202c6
lxs <893309066@qq.com>
2019-03-19 00:33:06 +0800

    kjlkol

--- src/main/java/com/lxs/graduate/config/WebSecurityConfig.java
+++ src/main/java/com/lxs/graduate/config/WebSecurityConfig.java
@@ -18,114 +18,114 @@ import org.springframework.security.web.access.DefaultWebInvocationPrivilegeEval
  * Created by sang on 16-12-22.
  */
 //@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    UserDetailsService customUserService(){ //注册UserDetailsService 的bean
+        return new CustomUserService();
+    }
+
+   @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+               //设置拦截规则
+                .antMatchers("/index","/ ","/css/**","/js/**","/img/**","/register","/redis/**","/websocket/**")
+                .permitAll()
+                .antMatchers("/user/**","/order/**","/cart/**","/product/**","/websocket/**").hasRole("USER")
+              .anyRequest()
+               .authenticated()
+               .and()
+              //开启默认登录页面
+              .formLogin()
+              .loginPage("/login")
+               //默认登录页面
+                .failureUrl("/login?error")
+               //默认登录成功跳转页面
+              .defaultSuccessUrl("/")
+                .permitAll()
+              .and()
+              //设置注销
+               .logout()
+              .logoutSuccessUrl("/")
+             .permitAll()
+               .and()
+              .rememberMe()
+              .and()
+              .csrf().disable();
+    }
+
+//   //4在内存中配置两个用户 wyf 和 wisely ,密码和用户名一致,角色是 USER
+//   @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//       auth
+//              .inMemoryAuthentication()
+//            .withUser("admin").password("admin ").roles("USER")
+//               .and()
+//                .withUser("lxs").password("lxs").roles("USER");
+//   }
+
+
+   @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserService()).passwordEncoder(new PasswordEncoder(){
+
+           @Override
+           public String encode(CharSequence rawPassword) {
+               return MD5Util.encode((String)rawPassword);
+           }
+
+           @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+               return encodedPassword.equals(MD5Util.encode((String)rawPassword));
+           }}); //users Details Service验证
+    }
+
+   @Override
+    public void configure(WebSecurity web) throws Exception {
+        //解决静态资源被拦截的问题
+        web.ignoring().antMatchers("/css/**","/js/**");
+   }
+
+}
+
+
+//@Configuration
 //@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-//    @Bean
-//    UserDetailsService customUserService(){ //注册UserDetailsService 的bean
-//        return new CustomUserService();
-//    }
-
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                //设置拦截规则
-//                .antMatchers("/index","/ ","/css/**","/js/**","/img/**","/register","/redis/**","/websocket/**")
-//                .permitAll()
-//                .antMatchers("/user/**","/order/**","/cart/**","/product/**").hasRole("USER")
-////                .anyRequest()
-////                .authenticated()
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/","/login").permitAll()//根路径和/login路径不拦截
+//                .anyRequest().authenticated()
 //                .and()
-//                //开启默认登录页面
 //                .formLogin()
-//                //默认登录页面
-//                .loginPage("/login")
-//                .failureUrl("/login?error")
-//                //默认登录成功跳转页面
-//                .defaultSuccessUrl("/chat")
+//                .loginPage("/login") //2登陆页面路径为/login
+//                .defaultSuccessUrl("/chat") //3登陆成功转向chat页面
 //                .permitAll()
 //                .and()
-//                //设置注销
 //                .logout()
-//                .logoutSuccessUrl("/")
-//                .permitAll()
-//                .and()
-//                .rememberMe()
-//                .and()
-//                .csrf().disable();
+//                .permitAll();
 //    }
 //
 //    //4在内存中配置两个用户 wyf 和 wisely ,密码和用户名一致,角色是 USER
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("admin").password("admin ").roles("USER")
+//        auth.
+//                inMemoryAuthentication()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .withUser("lxs").password(new BCryptPasswordEncoder().encode("lxs")).roles("USER")
 //                .and()
-//                .withUser("lxs").password("lxs").roles("USER");
-//    }
-
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(customUserService()).passwordEncoder(new PasswordEncoder(){
+//                .withUser("admin").password(new BCryptPasswordEncoder().encode("admin")).roles("USER");
 //
-//            @Override
-//            public String encode(CharSequence rawPassword) {
-//                return MD5Util.encode((String)rawPassword);
-//            }
 //
-//            @Override
-//            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-//                return encodedPassword.equals(MD5Util.encode((String)rawPassword));
-//            }}); //users Details Service验证
 //    }
-
+//    //5忽略静态资源的拦截
 //    @Override
 //    public void configure(WebSecurity web) throws Exception {
-//        //解决静态资源被拦截的问题
 //        web.ignoring().antMatchers("/css/**","/js/**");
 //    }
 //
 //}
-
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/","/login").permitAll()//根路径和/login路径不拦截
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login") //2登陆页面路径为/login
-                .defaultSuccessUrl("/chat") //3登陆成功转向chat页面
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }
-
-    //4在内存中配置两个用户 wyf 和 wisely ,密码和用户名一致,角色是 USER
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.
-                inMemoryAuthentication()
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("lxs").password(new BCryptPasswordEncoder().encode("lxs")).roles("USER")
-                .and()
-                .withUser("admin").password(new BCryptPasswordEncoder().encode("admin")).roles("USER");
-
-
-    }
-    //5忽略静态资源的拦截
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**","/js/**");
-    }
-
-}
-
+//
--- src/main/java/com/lxs/graduate/controller/WsController.java
+++ src/main/java/com/lxs/graduate/controller/WsController.java
@@ -1,11 +1,13 @@
 package com.lxs.graduate.controller;
 
+import com.lxs.graduate.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +20,12 @@ public class WsController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("/websocket/{name}")
-    public String webSocket(@PathVariable String name, Model model) {
+    @RequestMapping("/websocket")
+    public String webSocket( Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         try {
             logger.info("跳转到websocket的页面上");
-            model.addAttribute("username", name);
+            model.addAttribute("username", user.getUsername());
             return "websocket";
         } catch (Exception e) {
             logger.info("跳转到websocket的页面上发生异常，异常信息是：" + e.getMessage());
